3cc5031343db9b073f3660c2819bea695798c29f
faith-solverous <faithclm.solverous@gmail.com>
2026-01-12 14:41:59 +0800

    FAQs

--- solverouswebsite/src/pages/Home/home.js
+++ solverouswebsite/src/pages/Home/home.js
@@ -242,7 +242,7 @@ function Home() {
         </section>
 
         {/* FAQs Section */}
-        <section id="faqs" style={faqsBg} className=" flex flex-col md:flex-col lg:flex-row xl:flex-row justify-center items-center bg-no-repeat bg-cover bg-center h-[110vh] md:h-[63vh] lg:h-[90vh] xl:h-[92vh] 2xl:h-[55vh] space-y-[-2rem] md:space-y-10 lg:space-y-0 ">
+        <section id="faqs" style={faqsBg} className=" flex flex-col md:flex-col lg:flex-row xl:flex-row justify-center items-center bg-no-repeat bg-cover bg-center h-[110vh] md:h-[63vh] lg:h-[90vh] xl:h-[92vh] 2xl:h-[55vh] space-y-0 md:space-y-10 lg:space-y-0 ">
           <div className=" mr-[2.063rem] ml-[2.063rem] md:ml-[1.5rem] lg:ml-[7.4rem] mt-[3.5rem] md:mt-8 lg:mt-0 w-[84%] md:w-[71%] lg:w-fit space-y-6 md:space-y-4 justify-center items-start h-[32%] md:h-fit lg:h-[77%] ">
             <div className=" w-full md:w-full space-y-2 ">
               <h3 className="text-[1.5rem] md:text-3xl lg:text-[2rem] font-bold text-white ">Frequently <span className="text-[#0889f1]">Asked Questions</span></h3>
